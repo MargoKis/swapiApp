f9b3b29d8ac0f7328e1eeccc21ed65d3e5a44d6f
Marharyta <margaritakislajalemon@gmail.com>
2023-12-26 20:54:30 +0200

    small changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from './api/store.js'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="swapiApp">
       <Provider store={store}>
         <App />
       </Provider>
